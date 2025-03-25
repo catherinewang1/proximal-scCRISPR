8256e2185ed3e8047c7784f0b6c8d4887ca337e9
Catherine Wang <catheri2@andrew.cmu.edu>
2025-03-25 17:07:14 -0400

    CW: changed 1_processData sh script

--- papalexi-2021/analysis/1_processData/papalexi_1_processData_script.sh
+++ papalexi-2021/analysis/1_processData/papalexi_1_processData_script.sh
@@ -2,9 +2,9 @@
 Rscript 1.1_findimportantgenes_papalexi.R laptop 4000
 Rscript 1.2_normalize_papalexi.R laptop 4000
 Rscript 1.3_getchromosome_papalexi.R 
-Rscript 1.4_pca_umap_papalexi.R laptop 4000
+# Rscript 1.4_pca_umap_papalexi.R laptop 4000
 Rscript 1.5_dimred_plots_papalexi.R laptop 4000
-Rscript 1.4_pca_umap_papalexi.R laptop 2000
-Rscript 1.5_dimred_plots_papalexi.R laptop 2000
-Rscript 1.4_pca_umap_papalexi.R laptop 1000
-Rscript 1.5_dimred_plots_papalexi.R laptop 1000
+# Rscript 1.4_pca_umap_papalexi.R laptop 2000
+# Rscript 1.5_dimred_plots_papalexi.R laptop 2000
+# Rscript 1.4_pca_umap_papalexi.R laptop 1000
+# Rscript 1.5_dimred_plots_papalexi.R laptop 1000
