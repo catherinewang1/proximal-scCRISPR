e73c5d6f526934f2119c8923a27ce8c78d0e1034
Catherine Wang <catheri2@andrew.cmu.edu>
2025-04-10 17:01:28 -0400

    CW: added example for paths

--- papalexi-2021/analysis/PATHS.R
+++ papalexi-2021/analysis/PATHS.R
@@ -8,12 +8,14 @@
 data_dir = switch(DEVICE,
                'laptop'='C:/Users/Cathe/Documents/School/Genetic Hypothesis Testing using Negative Controls/genData/papalexi', 
                'desktop'='C:/Users/Catherine W/Documents/Research/genData/papalexi<UPDATE!>', 
-               'ubergenno'='/raid6/Catherine/papalexi')
+               'ubergenno'='/raid6/Catherine/papalexi',
+               'exampledevice'='/proximal-scCRISPR/data/papalexi')
 # location of intermediate save files/plots are written
 save_dir = switch(DEVICE,
                'laptop'='C:/Users/Cathe/Documents/School/Genetic Hypothesis Testing using Negative Controls/proximal-scCRISPR-github/proximal-scCRISPR/papalexi-2021/saves', 
                'desktop'='C:/Users/Catherine W/Documents/Research/DoubleBridge/saves/papalexi_saves<UPDATE!>', 
-               'ubergenno'='/raid6/Catherine/papalexi/papalexi_saves')
+               'ubergenno'='/raid6/Catherine/papalexi/papalexi_saves',
+               'exampledevice'='/proximal-scCRISPR/saves')
 # # location of utils code
 # CODE_DIR = switch(DEVICE,
 #                   'laptop'='C:/Users/Cathe/Documents/School/Genetic Hypothesis Testing using Negative Controls/Proximal/utils/dataProcessing', 
@@ -24,8 +26,10 @@ save_dir = switch(DEVICE,
 util_dir = switch(DEVICE,
                   'laptop'='C:/Users/Cathe/Documents/School/Genetic Hypothesis Testing using Negative Controls/proximal-scCRISPR-github/proximal-scCRISPR/utils', 
                   'desktop'='C:/Users/Catherine W/Documents/Research/DoubleBridge/code/utils', 
-                  'ubergenno'='/home/catheri2/proximal-scCRISPR-github/utils')
-                 # 'ubergenno' = 'http://ubergenno.stat.cmu.edu/home/catheri2/proximal-scCRISPR-github/utils')
+                  'ubergenno'='/home/catheri2/proximal-scCRISPR-github/utils',
+                  # 'ubergenno' = 'http://ubergenno.stat.cmu.edu/home/catheri2/proximal-scCRISPR-github/utils',
+                  'exampledevice'='/proximal-scCRISPR/utils')
+                 
 
 
 assertthat::assert_that(!is.null(data_dir), msg='first arg must be: laptop, desktop, or ubergenno')
